a8752ebc942f27b77430a4d92aa6d7f78b3313ad
mohitb07 <bmohit980@gmail.com>
2021-06-19 21:29:59 +0530

    Components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import PostCreation from "./PostCreation"
+import PostList from './PostList'
 
 export default () => {
     return (
         <div className="container">
             <h1>Create Posts</h1>
             <PostCreation/>
+            <hr/>
+            <h1>Posts</h1>
+            <PostList/>
         </div>
     )
 }
