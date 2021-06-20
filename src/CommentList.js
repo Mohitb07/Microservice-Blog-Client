788f6c6e5e73060cfeedbe03fca56a6dcfd7dd30
mohitb07 <bmohit980@gmail.com>
2021-06-20 12:27:18 +0530

    removed request

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,25 +1,14 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React from 'react'
 
-function CommentList({postId}) {
-    const [comments, setComments] = useState([])   
-    console.log('comment list post id ', postId)
-    useEffect(() => {
-        const fetchData = async() => {
-            const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-            console.log(res.data)
-            setComments(res.data)
-        }
-        fetchData()
-    },[])
+function CommentList({comments}) {
 
     const renderedComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
     })
     return (
-        <li>
+        <ul>
             {renderedComments}
-        </li>
+        </ul>
     )
 }
 
