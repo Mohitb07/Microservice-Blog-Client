8f1a3304eaca22767b910946c1600fcd2ae5101f
mohitb07 <bmohit980@gmail.com>
2021-09-07 20:37:45 +0530

    code refactored

--- src/PostCreation.js
+++ src/PostCreation.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react"
 import axios from 'axios'
 
-export default () => {
+export default function PostCreation(){
 
     const [title, setTitle] = useState('')
     
